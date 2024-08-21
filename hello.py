f57d4e648bed795a4d738c9b3f2e8dbb7652ae5e
Soliit <danistrokes04@gmail.com>
2024-08-21 16:28:24 +0000

    fist commit

--- hello.py
+++ hello.py
@@ -7,8 +7,14 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import requests
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+API_KEY = os.getenv("API_KEY")
+MAILGUN_DOMAIN = os.getenv("DOMAIN")
+MAILGUN_API_URL = f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages"
+RECIPIENTS = ['flaskaulasweb@zohomail.com', 'freitas.alves@aluno.ifsp.edu.br', 'daniel.a49.freitas@gmail.com']
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -20,6 +26,25 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+def send_email(username):
+
+
+    print(f"API KEY: {API_KEY}", flush=True)
+    print(f"DOMAIN: {MAILGUN_DOMAIN}", flush=True)
+    print(f"MAILGUN_API_URL: {MAILGUN_API_URL}", flush=True)
+
+    return requests.post(
+        MAILGUN_API_URL,
+        auth=("api", API_KEY),
+        data={
+            "from": f"Flasky App <mailgun@{MAILGUN_DOMAIN}>",
+            "to": RECIPIENTS,
+            "subject": "Novo usuário registrado!",
+            "text": f"Um novo usuário foi registrado: {username}"
+        }
+    )
 
 
 class Role(db.Model):
@@ -65,16 +90,29 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+
     if form.validate_on_submit():
+
         user = User.query.filter_by(username=form.name.data).first()
+
         if user is None:
+
             user = User(username=form.name.data)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+
         else:
+
             session['known'] = True
+
         session['name'] = form.name.data
+
+        send_email(user.username)
+
         return redirect(url_for('index'))
+
+    users = User.query.all()
+
     return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False))
+                           known=session.get('known', False), users=users)
